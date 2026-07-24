b9edaa2931a8ad570c85ae65833ae5e7d9592986
darknecrocities <parejasarronkian@gmail.com>
2026-07-24 19:41:49 +0800

    Optimize step question transitions in SignUpScreen to a butter-smooth 60fps FadeIn + Scale transition

--- lib/screens/signup/signup_screen.dart
+++ lib/screens/signup/signup_screen.dart
@@ -1538,69 +1538,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
                     Expanded(
                       child: AnimatedSwitcher(
-                        duration: const Duration(milliseconds: 600),
-                        switchInCurve: Curves.easeInOutCubic,
-                        switchOutCurve: Curves.easeInOutCubic,
-                        layoutBuilder: (Widget? currentChild, List<Widget> previousChildren) {
-                          return Stack(
-                            alignment: Alignment.topCenter,
-                            children: <Widget>[
-                              ...previousChildren,
-                              if (currentChild != null) currentChild,
-                            ],
-                          );
-                        },
+                        duration: const Duration(milliseconds: 300),
+                        switchInCurve: Curves.easeOutCubic,
+                        switchOutCurve: Curves.easeInCubic,
                         transitionBuilder: (Widget child, Animation<double> animation) {
-                          final isIncoming = (child.key as ValueKey<int>?)?.value == _currentStep;
-
-                          if (isIncoming) {
-                            final scale = Tween<double>(begin: 0.90, end: 1.0).animate(animation);
-                            final fade = Tween<double>(begin: 0.0, end: 1.0).animate(animation);
-                            return FadeTransition(
-                              opacity: fade,
-                              child: ScaleTransition(
-                                scale: scale,
-                                child: child,
+                          return FadeTransition(
+                            opacity: animation,
+                            child: ScaleTransition(
+                              scale: Tween<double>(begin: 0.96, end: 1.0).animate(
+                                CurvedAnimation(
+                                  parent: animation,
+                                  curve: Curves.easeOutCubic,
+                                ),
                               ),
-                            );
-                          } else {
-                            return AnimatedBuilder(
-                              animation: animation,
-                              builder: (context, _) {
-                                final splitProgress = 1.0 - animation.value;
-                                final leftOffset = Offset(-1.0 * splitProgress, 0.0);
-                                final rightOffset = Offset(1.0 * splitProgress, 0.0);
-
-                                return Stack(
-                                  children: [
-                                    // Left half of outgoing step question
-                                    FractionalTranslation(
-                                      translation: leftOffset,
-                                      child: ClipRect(
-                                        child: Align(
-                                          alignment: Alignment.centerLeft,
-                                          widthFactor: 0.5,
-                                          child: child,
-                                        ),
-                                      ),
-                                    ),
-
-                                    // Right half of outgoing step question
-                                    FractionalTranslation(
-                                      translation: rightOffset,
-                                      child: ClipRect(
-                                        child: Align(
-                                          alignment: Alignment.centerRight,
-                                          widthFactor: 0.5,
-                                          child: child,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          }
+                              child: child,
+                            ),
+                          );
                         },
                         child: SingleChildScrollView(
                           key: ValueKey<int>(_currentStep),
